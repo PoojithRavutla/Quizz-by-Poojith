9b4d5856fa72d6f5368cda4622d19bafb97d31ab
PoojithRavutla <89172464+PoojithRavutla@users.noreply.github.com>
2021-08-23 12:44:58 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,91 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+// create questions here
+var questions = [
+    new Question("What is the capital of Telangana?", ["Warangal", "Nizamabad","Khammam", "Hyderabad"], "Hyderabad"),
+    new Question("Who is the PM of India?", ["Amit shah", "Rahul Gandhi", "Narendra Modi", "KCR"], "Narendra Modi"),
+    new Question("Who is the captain of Indian Cricket team?", ["Rohith Sharma", "Virat Kohli","KL Rahul", "MS Dhoni"], "Virat Kohli"),
+    new Question("Who is the CM of Telangana?", ["KCR", "KTR", "Revanth Reddy", "Harish Rao"], "KCR"),
+    new Question("What is the capital of India?", ["New Delhi", "Mumbai", "Lucknow", "Hyderabad"], "New Delhi")
+];
+ 
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
